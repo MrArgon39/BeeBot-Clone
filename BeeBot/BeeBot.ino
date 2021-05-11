31d5655a3055ddca9115081dbba1be0696c623c8
MrArgon39 <62647685+MrArgon39@users.noreply.github.com>
2021-05-12 09:39:43 +1000

    Update BeeBot.ino
    Add basic Motor Functionality, Serial Control Only

--- BeeBot/BeeBot.ino
+++ BeeBot/BeeBot.ino
@@ -1,11 +1,25 @@
 //Clone Time
+int motorPin1 = 5;
+int motorPin2 = 3;
+
 
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  Serial.begin(9600);
+  while (! Serial);
+  Serial.println("Speed 0 to 255");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+  if (Serial.available())
+  {
+    int speed = Serial.parseInt();
+    if (speed >= 0 && speed <= 255)
+    {
+      analogWrite(motorPin1, speed);
+      analogWrite(motorPin2, speed);
+    }
+  }
 }
