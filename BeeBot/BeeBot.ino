3921e6d1ac843d82491ed4cc8db792d8fefdd320
MrArgon39 <62647685+MrArgon39@users.noreply.github.com>
2021-05-13 22:44:26 +1000

    Added ability to turn, and buttons to support that

--- BeeBot/BeeBot.ino
+++ BeeBot/BeeBot.ino
@@ -1,45 +1,61 @@
 //Clone Time
-int leftMotor = 5;
-int rightMotor = 3;
-int buttonPin = 2;
 
+int leftMotor = 6;
+int rightMotor = 5;
+int fwdButton = 3;
+int leftButton = 4;
+int rightButton = 2;
 
-void setup() {
-  pinMode(buttonPin, INPUT);
+void setup()
+{
+  pinMode(fwdButton, INPUT);
+  pinMode(leftButton, INPUT);
+  pinMode(rightButton, INPUT);
   pinMode(leftMotor, OUTPUT);
   pinMode(rightMotor, OUTPUT);
   Serial.begin(9600);
 }
 
-
-void loop() {
-  Serial.println(digitalRead(buttonPin));
-  if (digitalRead(buttonPin) == HIGH) {
+void loop()
+{
+  if (digitalRead(fwdButton) == HIGH)
+  {
     moveFwd();
+  }
+  if (digitalRead(leftButton) == HIGH)
+  {
+    turnLeft();
+  }
+  if (digitalRead(rightButton) == HIGH)
+  {
+    turnRight();
   }
 }
 
-void moveFwd() {
+void moveFwd()
+{
   analogWrite(leftMotor, 200);
   analogWrite(rightMotor, 200);
   delay(100);
   analogWrite(leftMotor, 0);
   analogWrite(rightMotor, 0);
-
 }
 
-void turnLeft() {
+void turnLeft()
+{
   analogWrite(leftMotor, 200);
   delay(100);
   analogWrite(leftMotor, 0);
 }
 
-void turnRight() {
+void turnRight()
+{
   analogWrite(rightMotor, 200);
   delay(100);
   analogWrite(rightMotor, 0);
 }
 
-void reverse() {
-  //No
+void reverse()
+{
+  //No, not gonna happen
 }
